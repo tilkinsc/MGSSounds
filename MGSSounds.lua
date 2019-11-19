64e79c985653a6c909aa780ccf603e3bad1145c0
Polite Kiwi <deocojo1@gmail.com>
2019-11-18 22:16:58 -0500

    Added more sounds

--- MGSSounds.lua
+++ MGSSounds.lua
@@ -26,23 +26,79 @@
 --]]
 
 
-local soundFile1 = "Interface\\AddOns\\MGSSounds\\mgsan.mp3"
-local soundFile2 = "Interface\\AddOns\\MGSSounds\\mgsdeath.mp3"
+local mgsan = "Interface\\AddOns\\MGSSounds\\mgsan.mp3"
+local mgsan_willplay, mgssan_handle
+
+local mgsdeath = "Interface\\AddOns\\MGSSounds\\mgsdeath.mp3"
+local mgsdeath_willplay, mgsdeath_handle
+
+local mgsring = "Interface\\AddOns\\MGSSounds\\mgsring.mp3"
+local mgsring_willplay, mgsring_handle
+
+local mgsdrop = "Interface\\AddOns\\MGSSounds\\mgsdrop.mp3"
+local mgsdrop_willplay, mgsdrop_handle
+
+local mgspickup = "Interface\\AddOns\\MGSSounds\\mgspickup.mp3"
+local mgspickup_willplay, mgspickup_handle
+
+local mgsmail = "Interface\\AddOns\\MGSSounds\\mgsmail.mp3"
+local mgsmail_willplay, mgsmail_handle
+
+local mgsopen = "Interface\\AddOns\\MGSSounds\\mgsopen.mp3"
+local mgsopen_willplay, mgsopen_handle
+
 
 local MGSSounds = CreateFrame("Frame", "MGSSounds")
-	MGSSounds:RegisterEvent("PLAYER_REGEN_DISABLED")
-	MGSSounds:RegisterEvent("PLAYER_DEAD")
+MGSSounds:RegisterEvent("PLAYER_REGEN_DISABLED")
+MGSSounds:RegisterEvent("PLAYER_DEAD")
+MGSSounds:RegisterEvent("TRADE_REQUEST")
+MGSSounds:RegisterEvent("TRADE_SHOW")
+MGSSounds:RegisterEvent("TRADE_REQUEST_CANCEL")
+MGSSounds:RegisterEvent("LOOT_OPENED")
+MGSSounds:RegisterEvent("LOOT_SLOT_CLEARED")
+MGSSounds:RegisterEvent("CHAT_MSG_WHISPER")
 
-	MGSSounds:SetScript("OnEvent", function(self, eventName, ...)
-		print(self, eventName, ...)
-		return self[eventName](self, eventName, ...)
-	end)
+MGSSounds:SetScript("OnEvent", function(self, eventName, ...)
+	print(self, eventName, ...)
+	return self[eventName](self, eventName, ...)
+end)
 
-function MGSSounds:PLAYER_DEAD(event)
-	PlaySoundFile(soundFile2, "Master")
-end
 
 function MGSSounds:PLAYER_REGEN_DISABLED(event)
-	PlaySoundFile(soundFile1, "Master")
+	mgsan_willplay, mgssan_handle = PlaySoundFile(mgsan, "Master")
 end
 
+function MGSSounds:PLAYER_DEAD(event)
+	mgsdeath_willplay, mgsdeath_handle = PlaySoundFile(mgsdeath, "Master")
+end
+
+function MGSSounds:TRADE_REQUEST(event)
+	mgsring_willplay, mgsring_handle = PlaySoundFile(mgsring, "Master")
+end
+
+function MGSSounds:TRADE_SHOW(event)
+	mgsopen_willplay, mgsopen_handle = PlaySoundFile(mgsopen, "Master")
+	if(mgsring_handle == nil) then
+		return
+	end
+	StopSound(mgsring_handle)
+end
+
+function MGSSounds:TRADE_REQUEST_CANCEL(event)
+	if(mgsring_handle == nil) then
+		return
+	end
+	StopSound(mgsring_handle)
+end
+
+function MGSSounds:LOOT_OPENED(event)
+	mgsdrop_willplay, mgsdrop_handle = PlaySoundFile(mgsdrop, "Master")
+end
+
+function MGSSounds:LOOT_SLOT_CLEARED(event)
+	mgspickup_willplay, mgspickup_handle = PlaySoundFile(mgspickup, "Master")
+end
+
+function MGSSounds:CHAT_MSG_WHISPER(event)
+	mgsmail_willplay, mgsmail_handle = PlaySoundFile(mgsmail, "Master")
+end
